feb5c8149cd8bef4ef29aa852c43037d45cd3cae
Jose Pena <jose.henrique.pena.goulart@gmail.com>
2024-02-19 10:55:00 -0300

    fix: test profile

--- src/test/java/com/unitech/classapi/config/testcontainer/TestContainerConfig.java
+++ src/test/java/com/unitech/classapi/config/testcontainer/TestContainerConfig.java
@@ -1,15 +1,13 @@
 package com.unitech.classapi.config.testcontainer;
 
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.*;
-import org.springframework.test.annotation.*;
+import org.springframework.test.context.*;
 import org.testcontainers.containers.MongoDBContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
+@ActiveProfiles("test")
 public class TestContainerConfig {
     @Bean
     @ServiceConnection
